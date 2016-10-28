5b80e8f8453d417ba09036beee190e07a6a39849
0807Jpatel <Jaykumar.patel@stonybrook.edu>
2016-10-28 00:13:43 -0400

    Cloning of TempCard with data

--- scripts/main.js
+++ scripts/main.js
@@ -25,6 +25,14 @@ $.ajax({
         $(data.internship).each(function(index, value){
             var clone =  $('#cardtemplate').clone().prop({ id: index}).insertBefore("#cardtemplate");
             clone.removeAttr('style'); 
+            var cl = clone.find('.companyLogo');
+            cl.css("background-image", "url(" + value.CompanyLogo + ")");
+            var cn = clone.find('.companyName');
+            cn.html(value.name);
+            var cn = clone.find('.tags');
+            $(value.Tag).each(function(index, value){
+                cn.append('<li class=\"tag ' + value + '\">' + value + "<\/li>");
+            });
         });
     }
 });
