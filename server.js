7125533055f4d90a6e9fd8490758f3cdd9ee1770
0807Jpatel <Jaykumar.patel@stonybrook.edu>
2016-11-09 20:15:02 -0500

    some minor changes

--- server.js
+++ server.js
@@ -13,84 +13,16 @@ var ref = firebase.database().ref();
 
 app.use(express.static(__dirname + '/styles'));
 app.use(express.static(__dirname + '/scripts'));
+app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname));
 app.get('/', function (req, res, next) {
   res.render('index.html');
 });
 
-app.get('/login', function (req, res) {
-  ref.push({
-    Username: "0807Jpatel",
-    id: 1023,
-    token: "1234asdf123e",
-    email: "0807Jpatel@gmail.com"
-  })
+app.get('/login', function(req, res, next){
+  res.render('login.html');
+})
 
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.get('/login', function (req, res) {
-//   var Mongo = mongo.MongoClient;
-//   var url = 'mongodb://localhost:27017/samplesite';
-//   Mongo.connect(url, function (err, db) {
-//     if (err) {
-//       console.log("you fucked up", err);
-//     } else {
-//       var collection = db.collection('student');
-//       collection.find({}).toArray(function(err, result){
-//          console.log(result.length);
-//           if(err){
-//               console.log("you fucked up again");
-//           }else if(result.length){
-//               console.log(collection.collectionName);
-//               console.log(result);
-//           }else{
-//               console.log("no doc");
-//           }
-//           db.close();
-//       });    
-
-//       // var user1 = { name: 'modulus admin'};
-//       // var user2 = { name: 'modulus user' };
-//       // var user3 = { name: 'modulus super admin'};
-
-//       // collection.insert([user1, user2, user3], function (err, result) {
-//       //   if (err) {
-//       //     console.log(err);
-//       //   } else {
-//       //     console.log(result);
-//       //   }
-//       //   //Close connection
-//       //   db.close();
-//       // });
-//     }
-//   });
-// });
-
-//app.use('/', router);
 
 app.listen(3000);
 
@@ -99,3 +31,38 @@ console.log("Running at Port 3000");
 app.listen(app.get('port'), function () {
   console.log('Express started press Ctrl-C to terminate');
 });
+
+
+
+// app.get('/', function (req, res) {
+  //  ref.push({
+  //   Username: "0807Jpatel",
+  //   id: 1023,
+  //   token: "1234asdf123e",
+  //   email: "0807Jpatel@gmail.com"
+  //  })
+  //   ref.push({
+  //   Username: "0807Rpatel",
+  //   id: 1024,
+  //   token: "1234asdf123f",
+  //   email: "0807Rpatel@gmail.com"
+  //  })
+  //   ref.push({
+  //   Username: "0807Jatel",
+  //   id: 1025,
+  //   token: "1234asdf123g",
+  //   email: "randomemail1@gmail.com"
+  //  })
+  //     ref.push({
+  //   Username: "080Jpatel",
+  //   id: 1026,
+  //   token: "1234asdf123h",
+  //   email: "randomemail2@gmail.com"
+  //  })
+    // res.render('login.html');
+    // ref.once('value')
+    //   .then(function(snap){
+    //     console.log(snap.val());
+    //   })
+
+// });
