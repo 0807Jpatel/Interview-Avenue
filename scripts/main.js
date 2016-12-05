ad11177189c0f02771e961455e709d88686d796f
0807Jpatel <Jaykumar.patel@stonybrook.edu>
2016-12-05 18:20:51 -0500

    enabled service worker

--- scripts/main.js
+++ scripts/main.js
@@ -368,12 +368,12 @@ function clearSearch() {
 
 
 
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('/serviceWorker.js').then(function (registration) {
-//         // Registration was successful
-//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//     }).catch(function (err) {
-//         // registration failed :(
-//         console.log('ServiceWorker registration failed: ', err);
-//     });
-// }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceWorker.js').then(function (registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function (err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}
