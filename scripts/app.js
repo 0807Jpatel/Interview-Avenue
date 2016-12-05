d8e047205d3b1adaba107e1a33e6454b2c9aad0a
0807Jpatel <Jaykumar.patel@stonybrook.edu>
2016-12-05 17:18:03 -0500

    Disabled enter on the search bar

--- scripts/app.js
+++ scripts/app.js
@@ -5,43 +5,66 @@ provider.addScope('')
 
 var googleLoginBtn2 = document.getElementById("googleLoginBtn");
 googleLoginBtn.addEventListener('click', e => {
-    firebase.auth().signInWithPopup(provider)
-    .catch(function (error) {
+    firebase.auth().signInWithRedirect(provider);
+
+
+    firebase.auth().getRedirectResult().then(function (result) {
+        if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+    }).catch(function (error) {
+        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log(errorMessage + '\n');
+        console.log(errorMessage);
+        // The email of the user's account used.
         var email = error.email;
-        console.log(email + '\n');
+        // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
+        // ...
     });
-    
+
+    // firebase.auth().signInWithRedirect(provider)
+    //     .catch(function (error) {
+    //         var errorCode = error.code;
+    //         var errorMessage = error.message;
+    //         console.log(errorMessage + '\n');
+    //         var email = error.email;
+    //         console.log(email + '\n');
+    //         var credential = error.credential;
+    //     });
+
 })
 
-firebase.auth().onAuthStateChanged(function(user){
-    if(user){
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
         counter++;
-        if(counter == 1){
-        googleLoginBtn2.style.display = "none";
+        if (counter == 1) {
+            googleLoginBtn2.style.display = "none";
 
-        console.log(user.displayName);
-        console.log(user.email);
-        console.log(user.uid);
-        
-        writeUserData(user.uid, user.displayName, user.email, user.photoURL);
-        console.log("counter = " + counter);
-        LoadUser();
+            console.log(user.displayName);
+            console.log(user.email);
+            console.log(user.uid);
+
+            writeUserData(user.uid, user.displayName, user.email, user.photoURL);
+            console.log("counter = " + counter);
+            LoadUser();
         }
-    }else{
+    } else {
         googleLoginBtn2.style.removeProperty('display');
     }
     return;
 })
 
 function writeUserData(userId, name, email, imageUrl) {
-  firebase.database().ref('Users/' + userId).update({
-    username: name,
-    email: email,
-    profile_picture : imageUrl,
-  });
-  console.log("User added to the database.");
+    firebase.database().ref('Users/' + userId).update({
+        username: name,
+        email: email,
+        profile_picture: imageUrl,
+    });
+    console.log("User added to the database.");
 }
