85284d58a8e0a9d62301412cda4d0782f05fd659
arslanms <arslanmsadiq@gmail.com>
2016-12-15 22:51:53 -0500

    Added logo

--- serviceWorker.js
+++ serviceWorker.js
@@ -14,6 +14,7 @@ var urlsToCache = [
   '/scripts/materialize.min.js',
   '/scripts/user.js',
   '/scripts/update.js',
+  '/scripts/about.js',
   '/views/content.html',
   '/views/hide.html',
   '/views/user.html',
