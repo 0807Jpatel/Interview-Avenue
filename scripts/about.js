9de51c2f1fffeee7deb0fe4b6c76008ca8eb8a26
Jay Patel <0807Jpatel@gmail.com>
2017-01-11 16:22:37 -0500

    added footer at the bottom of all pages

--- scripts/about.js
+++ scripts/about.js
@@ -2,5 +2,5 @@ function aboutContent()    {
     $("#selector").empty();
     $("#floatingButton").empty();
     $("#content").empty();
-    $("#content").append("<div style=\"text-align: center; margin-top:5%;   \"><img src=\"/images/icons/icon-128x128.png\"></img><h2>Internily</h2><p>the place to find all your internships</p><p>© 2016 Internily</p></div>");
+    $("#content").append("<div style=\"text-align: center; margin-top:5%; margin-bottom: -3.95%;\"><img src=\"/images/icons/icon-128x128.png\"></img><h2>Internily</h2><p>the place to find all your internships</p><p>© 2016 Internily</p></div>");
 }
